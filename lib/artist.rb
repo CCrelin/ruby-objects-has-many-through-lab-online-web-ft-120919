0e05a49b95cba85c6d9822166c72b5ddd09d938c
Cliff Crelin <cliff.crelin@gmail.com>
2020-01-12 05:17:52 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,2 +1,15 @@
 class Artist 
+  attr_accessor :name, :genre
   
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end
+  
+  def self.all
+    @@all
+  end
+  
+end 
